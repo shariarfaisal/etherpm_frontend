3dd3b91f5d07cdcfa7a264f63ad89cd71211e657
shariarfaisal <faisaljr623@gmail.com>
2020-11-06 14:50:43 +0600

    Updates

---
+++ precache-manifest.0422a84156ba88aa31e51002ea9d7af3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "20190ee276bab46eaafa9cb5c29ee8c3",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f3f281249f2b67c7f587",
+    "url": "/static/css/main.16d4eb5b.chunk.css"
+  },
+  {
+    "revision": "f63daea32dcdd731fcb5",
+    "url": "/static/js/2.3f3539eb.chunk.js"
+  },
+  {
+    "revision": "3d72a9594b3c4ca43da4afa8e1d7849a",
+    "url": "/static/js/2.3f3539eb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f3f281249f2b67c7f587",
+    "url": "/static/js/main.5b9a2f59.chunk.js"
+  },
+  {
+    "revision": "916d45e11bbfe12864af",
+    "url": "/static/js/runtime-main.94243213.js"
+  }
+]);
